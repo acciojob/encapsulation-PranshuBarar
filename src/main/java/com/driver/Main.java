20601b525faf66ad20304ae8a26f0d2efff5108e
91894@PRANSHU <pranshubarar1851996@gmail.com>
2023-01-13 19:40:49 +0530

    solution 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+//        System.out.println(obj.name); Message : java: name has private access in com.driver.RWOnly
+        obj.setName("Pranshu");
+    }
 }
